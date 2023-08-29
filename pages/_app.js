c4b65ac54799cf1a629bf2110a83773d7ef567d9
Doc Jaan Altosaar Li <jaan@onefact.org>
2023-08-29 08:19:08 -0600

    Update _app.js
    Signed-off-by: Doc Jaan Altosaar Li <jaan@onefact.org>

--- pages/_app.js
+++ pages/_app.js
@@ -21,8 +21,8 @@ import 'codemirror/lib/codemirror.css';
 
 import '../public/globals.css';
 
-const TITLE = 'One Fact';
-const DESCRIPTION = 'Our mission is to change global healthcare using open source principles.';
+const TITLE = 'Data Thinking';
+const DESCRIPTION = 'Open source courses for understanding AI, data, startups, and business.';
 const MARKDOC = `
 
 
